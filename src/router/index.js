2394245c1c58c60d73bcc2eb8b168f313ff1a00f
an <820125507@qq.com>
2020-03-11 11:44:50 +0800

    构建项目

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../views/Login/index.vue";
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "login"
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: () => import("../views/Register/index.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+    <h1>登录页面</h1>
+    <el-button type="success">成功按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
